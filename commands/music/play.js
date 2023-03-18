b958abe1ad28a7dd95afcc1ac5e5eccbbe285c57
yushi <ja.zuijderwijk80@gmail.com>
2023-03-18 20:54:18 +0100

    defer/reply fix

--- commands/music/play.js
+++ commands/music/play.js
@@ -28,7 +28,7 @@ module.exports = new Command({
 
         const search = interaction.options.getString('search');
 
-        console.error(search, client.player, interaction)
+        console.error(search)
 
         client.player.playSong(interaction, search, {}, async (err) => {
             if (err) {
