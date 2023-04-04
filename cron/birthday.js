2f2fa9cc4c0b45d34ec2d16cede107411dfd7f11
yushi <ja.zuijderwijk80@gmail.com>
2023-04-04 03:15:20 +0200

    + cronjob workers (bday command)

--- cron/birthday.js
+++ cron/birthday.js
@@ -15,34 +15,43 @@ module.exports = new Cron({
         const Birthday = client.database.get('birthday');
         const BirthdayChannel = client.database.get('birthday.channel')
 
-        for (const setting of await Birthday.find({ 'date': Date.now() })) {
-            const _channel = await BirthdayChannel.findOne({ 'guildId': setting.guildId });
+        for (const setting of await Birthday.find()) {
+            const d1 = new Date(Date.now());
+            const d2 = new Date(setting.date);
 
             if (
-                !_channel ||
-                !_channel.isEnabled
+                d1.getFullYear() === d2.getFullYear() &&
+                d1.getMonth() === d2.getMonth() &&
+                d1.getDate() === d2.getDate()
             ) {
-                return;
+                const _channel = await BirthdayChannel.findOne({ 'guildId': setting.guildId });
+
+                if (
+                    !_channel ||
+                    !_channel.isEnabled
+                ) {
+                    continue;
+                }
+
+                const guild = await client.guilds.fetch(setting.guildId);
+                const member = await guild.members.fetch(setting.userId);
+                const channel = await guild.channels.fetch(_channel.channelId);
+
+                if (
+                    !guild &&
+                    !member &&
+                    !channel
+                ) {
+                    continue;
+                }
+
+                const embed = new EmbedBuilder()
+                    .setTitle('Birthday Announcement')
+                    .setDescription(`Wishing ${userMention(member.user.id)} a happy birthday!`)
+                    .setTimestamp();
+
+                channel.send({ 'embeds': [embed] });
             }
-
-            const guild = await client.guilds.fetch(setting.guildId);
-            const member = await guild.members.fetch(setting.userId);
-            const channel = await guild.channels.fetch(_channel.channelId);
-
-            if (
-                !guild &&
-                !member &&
-                !channel
-            ) {
-                return;
-            }
-
-            const embed = new EmbedBuilder()
-                .setTitle('Birthday Announcement')
-                .setDescription(`Wishing ${userMention(member.user.id)} a happy birthday!`)
-                .setTimestamp();
-
-            channel.send({ 'embeds': [embed] });
         }
     }
 })
